a0e837f62f919748805a93882daed495bb7d0e25
Kenny <ekeneeze3@gmail.com>
2018-01-30 16:09:03 +0100

    Update Constants

--- app/src/main/java/com/example/ekene/cryptocompare/Constants.java
+++ app/src/main/java/com/example/ekene/cryptocompare/Constants.java
@@ -10,10 +10,5 @@ interface Constants {
         String EXTRA_BTC_RATE = "extra btc";
         String EXTRA_ETH_RATE = "extra eth";
 
-        int ORDER_ALPHABETICAL = 1;
-        int ORDER_BY_RATE = 2;
-
-        String INVALID_CONVERSION = "Invalid conversion value provided";
-        String REFRESH_ERROR = "Could not refresh";
     }
 
